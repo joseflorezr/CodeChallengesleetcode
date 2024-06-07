51b751bf44bcdba7549ed77b14ae7eec3feca52c
Jose Florez <jose.florezr@perficient.com>
2024-06-07 16:53:39 -0500

    Excersises 9 and 10

---
+++ ChallengeImplementation/Cases/PalindromeNumber.cs
@@ -0,0 +1,57 @@
+﻿using ChallengeImplementation.Model;
+
+namespace ChallengeImplementation.Cases
+{
+		/// <summary>
+		/// Given an integer x, return true if x is a 
+		/// palindrome, and false otherwise.
+		/// </summary>
+		/// <example>
+		/// Example 1:
+		/// Input: x = 121
+		/// Output: true
+		/// Explanation: 121 reads as 121 from left to right and from right to left.
+		/// Example 2:
+		/// Input: x = -121
+		/// Output: false
+		/// Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+		/// Example 3:
+		/// Input: x = 10
+		/// Output: false
+		/// Explanation: Reads 01 from right to left.Therefore it is not a palindrome.
+		/// Constraints:
+		/// -231 <= x <= 231 - 1
+		/// </example>
+		public class PalindromeNumber:IOption
+		{
+				readonly IReader reader;
+				readonly string input;
+        public PalindromeNumber(IReader reader)
+        {
+            this.reader = reader;
+						input=reader.ReadInput("Write the input number");
+        }
+        public void Execute()
+				{
+						int inputNumber = 0;
+						if (string.IsNullOrEmpty(input)||!int.TryParse(input,out inputNumber))
+						{
+								reader.WriteOutput("Choose a valid input");
+						}
+						else
+						{
+								reader.WriteOutput($"Result:{IsPalindrome(inputNumber)}");
+						}
+				}
+				static bool IsPalindrome(int x)
+				{
+						var stringArray = x.ToString().ToCharArray();
+						var reverse = string.Empty;
+						for (int i = stringArray.Length-1;i >= 0;i--) {
+								reverse = $"{reverse}{stringArray[i]}";
+						}
+						return x.ToString().Equals(reverse);
+				}
+
+		}
+}
---
+++ ChallengeImplementation/Cases/RegularExpressionMatching.cs
@@ -0,0 +1,92 @@
+﻿using ChallengeImplementation.Model;
+
+namespace ChallengeImplementation.Cases
+{
+		/// <summary>
+		/// Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
+		/// '.' Matches any single character.
+		/// '*' Matches zero or more of the preceding element.
+		/// The matching should cover the entire input string (not partial).
+		/// </summary>
+		/// <example>
+		/// Example 1:
+		/// Input: s = "aa", p = "a"
+		/// Output: false
+		/// Explanation: "a" does not match the entire string "aa".
+		/// Example 2:
+		/// Input: s = "aa", p = "a*"
+		/// Output: true
+		/// Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+		/// Example 3:
+		/// Input: s = "ab", p = ".*"
+		/// Output: true
+		/// Explanation: ".*" means "zero or more (*) of any character (.)".
+		/// Constraints:
+		/// 
+		/// 1 <= s.length <= 20
+		/// 1 <= p.length <= 20
+		/// </example>
+		public class RegularExpressionMatching:IOption
+		{
+				readonly IReader reader;
+				readonly string pattern;
+				readonly string expression;
+				public RegularExpressionMatching(IReader reader)
+				{
+						this.reader = reader;
+						expression= reader.ReadInput("Input to evaluate:");
+						pattern = reader.ReadInput("Write the pattern:");
+				}
+				public void Execute()
+				{
+						reader.WriteOutput($"Result:[{IsMatch(expression,pattern)}]");
+				}
+				public bool IsMatch(string s,string p)
+				{
+						if(string.IsNullOrWhiteSpace(s) || string.IsNullOrWhiteSpace(p)) 
+								return false;
+						if(s==p)
+								return true;
+						if (s.Length != p.Length)
+								return false;
+						const char single = '.';
+						const char all = '*';
+						
+						
+						var expressionArray = s.ToCharArray();
+						var patternArray = p.ToCharArray();
+
+            for (int i = 0; i < expressionArray.Length; i++)
+            {
+								if (expressionArray[i] != patternArray[i] && (patternArray[i] != single
+										&& patternArray[i] != all))
+								{
+										return false;
+								}
+								else
+								{
+										if (patternArray[i] == single)
+										{
+												if (i - 1 < 0)
+												{
+														return false;
+												}
+												else if (expressionArray[i-1]!= expressionArray[i])
+												{
+														return false;
+												}
+										}
+										else if (patternArray[i] == all)
+										{
+												var subString = s.Substring(0,i);
+												if (!subString.Contains(expressionArray[i]))
+												{
+														return false;
+												}
+										}
+								}
+            }
+            return true;
+				}
+		}
+}
